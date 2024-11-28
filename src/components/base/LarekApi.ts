6f3b0192b85fc7fbbaba3fb8f39395616a08d4bf
Alexander Stepkin <sascha.stepkin@gmail.com>
2024-11-28 22:58:25 +0500

    Part2.write code to implement the functionality

---
+++ src/components/base/LarekApi.ts
@@ -0,0 +1,25 @@
+import { Api, ApiListResponse } from './api';
+import { IProduct, IOrderAnswer, IOrder } from '../../types/index';
+import { ILarekApi } from '../../types/index';
+
+export class LarekApi extends Api implements ILarekApi {
+	readonly cdn: string;
+
+	constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+		super(baseUrl, options);
+		this.cdn = cdn;
+	}
+
+	getProductList(): Promise<IProduct[]> {
+		return this.get('/product').then((data: ApiListResponse<IProduct>) =>
+			data.items.map((item) => ({
+				...item,
+				image: this.cdn + item.image,
+			}))
+		);
+	}
+
+	orderProduct(value: IOrder): Promise<IOrderAnswer> {
+		return this.post('/order', value).then((data: IOrderAnswer) => data);
+	}
+}
